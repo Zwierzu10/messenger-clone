285e51491175ea60959d40f8e799f63df4309d8b
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2025-02-23 18:55:08 +0100

    scrolling down

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -3,9 +3,7 @@ import { collection, query, addDoc, serverTimestamp, onSnapshot, orderBy, where 
 import { firestore } from "../../firebaseConfig";
 import { Send } from "lucide-react";
 
-import { useState, useEffect } from "react";
-
-
+import { useState, useEffect, useRef } from "react";
 import { useUser } from "@clerk/nextjs";
 
 interface UserType {
@@ -18,7 +16,6 @@ interface SelectedUserProps {
   selectedUser: UserType | null;
 }
 
-
 interface Message {
   id: string;
   senderId: string;
@@ -27,13 +24,12 @@ interface Message {
   timestamp: any;
 }
 
-
-
 const Chat: React.FC<SelectedUserProps> = ({ selectedUser }) => {
-
   const [inputValue, setInputValue] = useState("");
   const [messages, setMessages] = useState<Message[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
+  
   const { user } = useUser();
   const currentUserId = user?.id;
 
@@ -62,17 +58,15 @@ const Chat: React.FC<SelectedUserProps> = ({ selectedUser }) => {
     });
 
     return () => unsubscribe();
-  }, [selectedUser]);
+  }, [selectedUser, currentUserId]);
 
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
 
-
+  }, [messages]);
 
   const sendMessage = async () => {
-    console.log("first")
-    if (!user || !selectedUser || inputValue.trim() === ""){
-      console.log(user, selectedUser, inputValue);
-      return;
-    } 
+    if (!user || !selectedUser || inputValue.trim() === "") return;
 
     await addDoc(collection(firestore, "messages"), {
       senderId: user.id,
@@ -81,38 +75,59 @@ const Chat: React.FC<SelectedUserProps> = ({ selectedUser }) => {
       text: inputValue,
       timestamp: serverTimestamp(),
     });
+
     setInputValue("");
-  }
-
-
+  };
 
   return (
     <div className="CHAT-DIV w-4/5 h-screen bg-gray-100">
       {selectedUser ? (
         <div className="SELECTED-CHAT w-full h-full flex flex-col justify-start items-start">
-          <div className=" w-full h-[10%] bg-gray-200 flex justify-start items-center border-b-2 border-gray-300">
+          <div className="w-full h-[10%] bg-gray-200 flex justify-start items-center border-b-2 border-gray-300">
             <h1 className="text-4xl text-black ml-[5%]">
               {selectedUser.name} {selectedUser.surname}
             </h1>
           </div>
 
-            <div className="CHAT-DIV w-full h-[90%] bg-gray-100 flex flex-col justify-end items-start">
-              <div className="MESSAGES-CONTAINER w-full h-[full] overflow-auto">
-                {messages.map((message) => (
-                      <div key={message.id} className={`w-full h-auto flex justify-${message.senderId === currentUserId ? "end" : "start"} items-center p-1`}>
-                          <div className={`bg-${message.senderId === currentUserId ? "blue-400" : "gray-200"} rounded-xl p-4`}>
-                              <h1 className="text-black">{message.text}</h1>
-                          </div>
-                      </div>
-                  ))}
-              </div>
-
-                <div className="w-full h-[10%] bg-transparent flex justify-start items-center p-4">
-                    <input type="text" className="w-4/5 h-full rounded-full text-black placeholder:text-black p-3" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-                    <button className="w-[5%] flex justify-center items-center rotate-45" onClick={sendMessage}><Send size={32} className="text-black"/></button>
+          <div className="CHAT-DIV w-full h-[90%] bg-gray-100 flex flex-col justify-end items-start">
+            <div className="MESSAGES-CONTAINER w-full h-full overflow-auto">
+              {messages.map((message) => (
+                
+             
+              
+                <div
+                  key={message.id}
+                  className={`w-full h-auto flex justify-${message.senderId === currentUserId ? "end" : "start"} items-center p-1`}
+                >
+                  <div className={`${message.senderId === currentUserId ? "bg-blue-400 text-white" : "bg-gray-200 text-black"} rounded-xl p-4 cursor-default`}>
+                    <h1>{message.text}</h1>
+                    </div>
                 </div>
+                
+                
+                
+                
+                ))}
+              <div ref={messagesEndRef} />
             </div>
 
+            <div className="w-full h-[10%] bg-transparent flex justify-start items-center p-4">
+              <input
+                type="text"
+                className="w-4/5 h-full rounded-full text-black placeholder:text-black p-3"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    sendMessage();
+                  }
+                }}
+              />
+              <button className="w-[5%] flex justify-center items-center rotate-45" onClick={sendMessage}>
+                <Send size={32} className="text-black" />
+              </button>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="flex flex-col justify-center items-center">
